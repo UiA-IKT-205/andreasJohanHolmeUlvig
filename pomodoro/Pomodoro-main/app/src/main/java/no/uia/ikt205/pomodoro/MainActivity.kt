512c14c56fd3a69954eab59268780f64206f71a6
Andreas Johan Holme Ulvig <ajulvi18@uia.no>
2021-01-26 19:24:35 +0100

    added buttons and fixed a bug

--- pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -14,35 +14,71 @@ class MainActivity : AppCompatActivity() {
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
     lateinit var coutdownDisplay:TextView
+    lateinit var thirtyButton:Button
+    lateinit var sixtyButton:Button
+    lateinit var ninetyButton:Button
+    lateinit var oneTwentyButton:Button
 
-    val timeToCountDownInMs = 5000L
-    val timeTicks = 1000L
+    var timeToCountDownInMs = 5000L
+    var timeTicks = 1000L
+    var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener(){
+            startCountDown(it)
+        }
+        thirtyButton = findViewById<Button>(R.id.thirtyMinutes)
+        thirtyButton.setOnClickListener(){
+            thirtyMinutes()
+        }
+        sixtyButton = findViewById<Button>(R.id.sixtyMinutes)
+        sixtyButton.setOnClickListener(){
+            sixtyMinutes()
+        }
+        ninetyButton = findViewById<Button>(R.id.ninetyMinutes)
+        ninetyButton.setOnClickListener(){
+            ninetyMinutes()
+        }
+        oneTwentyButton = findViewById<Button>(R.id.oneTwentyMinutes)
+        oneTwentyButton.setOnClickListener(){
+            oneTwentyMinutes()
+        }
+        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
     }
 
     fun startCountDown(v: View){
-
+        if(isRunning){
+            timer.cancel()
+        }
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                isRunning = false
             }
 
             override fun onTick(millisUntilFinished: Long) {
                updateCountDownDisplay(millisUntilFinished)
             }
         }
-
+        isRunning = true
         timer.start()
+    }
+    fun thirtyMinutes(){
+        timeToCountDownInMs = 1800000L
+    }
+    fun sixtyMinutes(){
+        timeToCountDownInMs = 3600000L
+    }
+    fun ninetyMinutes(){
+        timeToCountDownInMs = 5400000L
+    }
+    fun oneTwentyMinutes(){
+        timeToCountDownInMs = 7200000L
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
