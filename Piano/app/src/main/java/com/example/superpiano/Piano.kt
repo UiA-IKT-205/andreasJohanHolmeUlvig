364d000e20f917dc8e97aeecb487908a9549dafa
Andreas Johan Holme Ulvig <ajulvi18@uia.no>
2021-04-30 20:46:32 +0200

    part 1 done

--- Piano/app/src/main/java/com/example/superpiano/Piano.kt
+++ Piano/app/src/main/java/com/example/superpiano/Piano.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.superpiano.data.Note
 import com.example.superpiano.databinding.FragmentPianoBinding
 import kotlinx.android.synthetic.main.fragment_piano.*
+import kotlinx.android.synthetic.main.fragment_piano.view.*
 
 class Piano : Fragment() {
 
@@ -14,6 +16,7 @@ class Piano : Fragment() {
     private val binding get() = _binding!!
 
     private val fullTones = listOf("C","D","E","F","G","A","B","C2","D2","E2","F2","G2")
+    private val halfTones = listOf("C#","D#","F#","G#","A#","C#2","D#2","F#2","G#2")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class Piano : Fragment() {
 
         val fm = childFragmentManager
         val ft = fm.beginTransaction()
+        private val score:MutableList<Note> = mutableListOf<Note>
 
         fullTones.forEach{
             val fullTonePianoKey = FullTonePianoKeyFragment.newInstance(it)
@@ -38,10 +42,27 @@ class Piano : Fragment() {
             fullTonePianoKey.onKeyUp = {
                 println("piano key up $it")
             }
+            ft.add(view.fullTonePianoKeys.id, fullTonePianoKey, "note_$it")
 
-            ft.add(view.pianoKeys.id,fullTonePianoKey,"note $it")
+      //      ft.add(view.pianoKeys.id,fullTonePianoKey,"note $it")
         }
+        halfTones.forEach { it ->
+            val halfTonePianoKey = HalfTonePianoKeyFragment.newInstance(it)
 
-        return inflater.inflate(R.layout.fragment_piano, container, false)
+            halfTonePianoKey.onKeyDown = { note ->
+                println("Piano key down $note")
+            }
+
+            halfTonePianoKey.onKeyUp = { note ->
+                println("Piano key up $note")
+            }
+
+            // View is possibly not instanced (possibly move to "onViewCreated")
+            ft.add(view.halfToneKeysLayout.id, halfTonePianoKey, "note_$it")
+
+        }
+        ft.commit()
+
+        return view
     }
 }
